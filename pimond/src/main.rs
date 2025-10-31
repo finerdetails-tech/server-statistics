bfd14bb36c3a5608d47448c957352b7517166c9b
FinerDetails <erkka.finer@hotmail.fi>
2025-10-31 21:07:15 +0200

    `send_http_request`-function

--- pimond/src/main.rs
+++ pimond/src/main.rs
@@ -1,26 +1,40 @@
 mod get_metrics;
 mod helpers;
 
-use get_metrics::{get_cpu_info, get_mem_info};
+use get_metrics::{Metric, get_cpu_info, get_mem_info};
 
+use http::StatusCode;
 use std::error::Error;
 
-fn generate_json() -> Result<String, Box<dyn Error>> {
+fn combine_metrics() -> Result<Vec<Metric>, Box<dyn Error>> {
     let cpu_info = get_cpu_info()?;
     let mem_info = get_mem_info()?;
 
-    let mut all_metrics = Vec::new();
-    all_metrics.extend(cpu_info);
-    all_metrics.extend(mem_info);
+    let mut combined_metrics = Vec::new();
+    combined_metrics.extend(cpu_info);
+    combined_metrics.extend(mem_info);
 
-    let json = serde_json::to_string(&all_metrics)?;
+    Ok(combined_metrics)
+}
 
-    Ok(json)
+async fn send_http_request(metrics: Vec<Metric>) -> Result<StatusCode, Box<dyn Error>> {
+    let client = reqwest::Client::new();
+    let res = client
+        .post("http://127.0.0.1:8080/post")
+        .json(&metrics)
+        .send()
+        .await?;
+
+    Ok(res.status())
 }
 
 fn main() {
-    match generate_json() {
-        Ok(json) => println!("{}", json),
-        Err(e) => eprintln!("Error generating JSON: {}", e),
-    }
+    let metrics = match combine_metrics() {
+        Ok(m) => m,
+        Err(e) => {
+            eprintln!("Error collecting metrics: {}", e);
+            return;
+        }
+    };
+    send_http_request(metrics);
 }
