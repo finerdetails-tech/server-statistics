f701c20f8beec5d26c22fa3929ccd8811c9d0b2c
FinerDetails <erkka.finer@hotmail.fi>
2025-11-09 20:48:57 +0200

    converted all values to use `MetricValue`

--- pimond/src/get_metrics.rs
+++ pimond/src/get_metrics.rs
@@ -7,9 +7,17 @@ use std::thread::sleep;
 use std::time::Duration;
 
 #[derive(Serialize, Debug)]
+pub enum MetricValue {
+    Float(f64),
+    Int(u64),
+    Str(String),
+    Bool(bool),
+}
+
+#[derive(Serialize, Debug)]
 pub struct Metric {
     pub name: String,
-    pub value: f64,
+    pub value: MetricValue,
     pub timestamp: i64,
 }
 
@@ -41,10 +49,10 @@ pub async fn get_cpu_info() -> Result<Vec<Metric>, Box<dyn Error>> {
     let inactive_delta = cpu_inactive_2 - inactive_1;
 
     let cpu_usage_percent_raw = (1.00 - (inactive_delta / total_delta)) * 100.0;
-    let cpu_usage_percent_rounded_value = (cpu_usage_percent_raw * 10.0).round() / 10.0;
+    let cpu_usage_percent_rounded = (cpu_usage_percent_raw * 10.0).round() / 10.0;
     let cpu_usage = Metric {
         name: "cpu_usage_percent".to_string(),
-        value: cpu_usage_percent_rounded_value,
+        value: MetricValue::Float(cpu_usage_percent_rounded),
         timestamp: current_unix_time(),
     };
 
@@ -61,28 +69,30 @@ pub async fn get_mem_info() -> Result<Vec<Metric>, Box<dyn Error>> {
         .nth(1)
         .ok_or("Could not read value in mem_total_line")?
         .parse::<f64>()?;
+
     let mem_available_kb = mem_available_line
         .nth(1)
         .ok_or("Could not read value in mem_available_line")?
         .parse::<f64>()?;
 
-    let mem_usage_percent_raw = (1.0 - (mem_available_kb / mem_total_kb)) * 100.0;
-    let mem_usage_percent_rounded_value = (mem_usage_percent_raw * 10.0).round() / 10.0;
+    let mem_usage_percent_raw = ((1.0 - (mem_available_kb / mem_total_kb)) * 100.0) as f64;
+
+    let mem_usage_percent_rounded = (mem_usage_percent_raw * 10.0).round() / 10.0;
 
     let mem_total = Metric {
         name: "mem_total_kb".to_string(),
-        value: mem_total_kb,
+        value: MetricValue::Int(mem_total_kb as u64),
         timestamp: current_unix_time(),
     };
 
     let mem_available_kb = Metric {
         name: "mem_available_kb".to_string(),
-        value: mem_available_kb,
+        value: MetricValue::Int(mem_available_kb as u64),
         timestamp: current_unix_time(),
     };
     let mem_usage_percent = Metric {
         name: "mem_usage_percent".to_string(),
-        value: mem_usage_percent_rounded_value,
+        value: MetricValue::Float(mem_usage_percent_rounded),
         timestamp: current_unix_time(),
     };
 
@@ -92,11 +102,11 @@ pub async fn get_mem_info() -> Result<Vec<Metric>, Box<dyn Error>> {
 pub async fn get_temp_info() -> Result<Vec<Metric>, Box<dyn Error>> {
     let temp_line = file_lines_by_number("/sys/class/thermal/thermal_zone0/temp", &[0])?;
     let temp_field = temp_line[0].trim();
-    let temp_value = temp_field.parse::<f64>()? / 1000.0;
+    let temp = temp_field.parse::<f64>()? / 1000.0;
 
     Ok(Vec::from([Metric {
         name: "cpu_temp_celsius".to_string(),
-        value: temp_value,
+        value: MetricValue::Float(temp),
         timestamp: current_unix_time(),
     }]))
 }
@@ -112,27 +122,28 @@ pub async fn get_disk_info() -> Result<Vec<Metric>, Box<dyn Error>> {
         return Err("Failed to get filesystem statistics".into());
     }
 
-    let disk_total_kb_value = stats.f_blocks as f64 * stats.f_frsize as f64;
-    let disk_free_kb_value = stats.f_bfree as f64 * stats.f_frsize as f64;
-    let disk_used_kb_value = disk_total_kb_value - disk_free_kb_value;
-    let disk_used_percent_raw = (disk_used_kb_value / disk_total_kb_value) * 100.0;
-    let disk_used_percent_value = (disk_used_percent_raw * 10.0).round() / 10.0;
+    let disk_total_kb = stats.f_blocks * stats.f_frsize;
+    let disk_free_kb = stats.f_bfree * stats.f_frsize;
+    let disk_used_kb = disk_total_kb - disk_free_kb;
+    let disk_used_percent_raw = (disk_used_kb / disk_total_kb) as f64 * 100.0;
+
+    let disk_used_percent = (disk_used_percent_raw * 10.0).round() / 10.0;
 
     let disk_total_kb = Metric {
         name: "disk_total_kb".to_string(),
-        value: disk_total_kb_value,
+        value: MetricValue::Int(disk_total_kb),
         timestamp: current_unix_time(),
     };
 
     let disk_used_kb = Metric {
         name: "disk_used_kb".to_string(),
-        value: disk_used_kb_value,
+        value: MetricValue::Int(disk_used_kb),
         timestamp: current_unix_time(),
     };
 
     let disk_used_percent = Metric {
         name: "disk_used_percent".to_string(),
-        value: disk_used_percent_value,
+        value: MetricValue::Float(disk_used_percent),
         timestamp: current_unix_time(),
     };
 
