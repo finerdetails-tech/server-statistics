55b862878c83917cb0d348988aa674d1d4429bd4
FinerDetails <erkka.finer@hotmail.fi>
2025-11-23 21:21:47 +0200

    finished `GetMetrics`-function

--- backend/database/database_helpers.go
+++ backend/database/database_helpers.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"time"
+
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
@@ -14,7 +16,25 @@ type Metric struct {
 	Value     string
 }
 
+func unixTimeMonthAgo() int64 {
+	return time.Now().AddDate(0, -1, 0).Unix()
+}
+
+func GetMetrics() []Metric {
+	var metrics []Metric
+	if err != nil {
+		panic("failed to connect database, " + err.Error())
+	}
+	if res := db.Where("timestamp > ?", unixTimeMonthAgo()).Find(&metrics); res.Error != nil {
+		panic("failed to get metrics, " + res.Error.Error())
+	}
+	return metrics
+}
+
 func InsertMetric(m Metric) Metric {
+	if err != nil {
+		panic("failed to connect database, " + err.Error())
+	}
 	if res := db.Create(&m); res.Error != nil {
 		panic("failed to insert metric, " + res.Error.Error())
 	}
