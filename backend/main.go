aa1d45d0ca5648dfaad2f455f8e327e84112e121
FinerDetails <erkka.finer@hotmail.fi>
2025-11-26 18:57:50 +0200

    `cleanupExpiredMetrics` -goroutine

--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"api/database"
 	"api/router"
+	"time"
 )
+
+func cleanupExpiredMetrics() {
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		database.RemoveExpiredMetrics()
+	}
+}
 
 func main() {
 	database.InitDb()
+	go cleanupExpiredMetrics()
 	router.InitRouter()
 }
