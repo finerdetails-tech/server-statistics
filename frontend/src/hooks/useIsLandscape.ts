3855de60aaa8dcfbd0fb9f888cd5685211a9fde9
Finer_Details <erkka.finer@hotmail.fi>
2026-05-16 02:49:08 +0300

    fixing `useIsLandscape` recreation/cleanup

--- frontend/src/hooks/useIsLandscape.ts
+++ frontend/src/hooks/useIsLandscape.ts
@@ -1,9 +1,17 @@
-import { useState } from 'preact/hooks'
+import {
+  useEffect, useState
+} from 'preact/hooks'
 function useIsLandscape () {
   const mediaQueryList = window.matchMedia("(orientation: landscape)")
   const [ isLandscape, setIsLandscape ] = useState(mediaQueryList.matches)
 
-  mediaQueryList.addEventListener("change", (e) => setIsLandscape(e.matches))
+  useEffect(() => {
+    const handleChange = (e: MediaQueryListEvent) => setIsLandscape(e.matches)
+    mediaQueryList.addEventListener("change", handleChange)
+    return () => {
+      mediaQueryList.removeEventListener("change", handleChange)
+    }
+  }, [])
 
   return isLandscape
 }
